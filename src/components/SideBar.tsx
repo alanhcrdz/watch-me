93f7d6d63cdb4cf78c953d79e1d8d0bb89f8f9ae
alanhcrdz <alanhcrdz@gmail.com>
2021-03-26 17:11:07 -0300

    first commit

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,44 @@
+import { useContext } from 'react';
+
+
+
+import '../styles/global.scss';
+
+import '../styles/sidebar.scss';
+import '../styles/content.scss';
+import { Button } from './Button';
+import { DataContext } from '../DataContext';
+
+
+
+
 export function SideBar() {
-  // Complete aqui
+
+  const { genres, selectedGenreId, handleClickButton } = useContext(DataContext);
+
+
+
+
+
+
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+
+  )
 }
